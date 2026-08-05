79a5056f17b36921449cde95d1390c2b6d7da53c
qla.dev <kulasin.nedim@qla.dev>
2026-08-05 19:44:36 +0200

    mw

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::redirect(
     '/apple-download',
     'https://apps.apple.com/us/app/varalica-imposter-igrica/id6784401796'
-);
+)->withoutMiddleware([
+    \Illuminate\Session\Middleware\StartSession::class,
+    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+]);
 
 Route::get('/', fn () => response()->file(public_path('dist/index.html')))
     ->withoutMiddleware([
